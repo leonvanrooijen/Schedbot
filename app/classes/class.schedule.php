a14b5217e7b3c24755c04424c8a8c945ded4a931
BuildTools <unconfigured@null.spigotmc.org>
2017-09-11 21:22:01 +0200

    Changed time to UTC

--- app/classes/class.schedule.php
+++ app/classes/class.schedule.php
@@ -10,6 +10,7 @@ class Schedule {
   }
 
   function checkTime() {
+    date_default_timezone_set('Europe/Amsterdam');
     if(!in_array(date("H:i"), $this->time_data)) {
       return false;
     }
@@ -17,6 +18,7 @@ class Schedule {
   }
 
   function sendClassroom() {
+    date_default_timezone_set('UTC');
     $time = time();   //Calculate the time of the lessons
     $start_lesson = (ceil(time()/300)*300);
     $end_lesson = ($start_lesson)+60;
@@ -29,7 +31,24 @@ class Schedule {
 
     foreach($decodedZermeloResult['response']['data'] as $lesson) {
       if($lesson['cancelled'] == false && $lesson['valid'] == true) { //Send the message to Noury
-        return file_get_contents('https://api.telegram.org/bot ' . $this->telegram_token . '/sendMessage?chat_id=429246296&text=Hey%20Noury!%20Je%20hebt%20zometeen%20' . $lesson["subjects"][0] . '%20in%20lokaal%20' . $lesson['locations'][0] .'.%20📚⏰');
+        $curl_handle = curl_init();
+
+        $search  = array('lo', 'fatl', 'wisa', 'ckv', 'netl', 'entl', 'econ', 'mo', 'ges', 'maat', 'ml', 's4hwia');
+        $replace = array('gym', 'Frans', 'wiskunde', 'CKV', 'Nederlands', 'Engels', 'economie', 'M&O', 'geschiedenis', 'maatschappijleer', 'mentorles', 'wiskunde');
+
+
+          /*
+          * Groep: -244481806   | Private: 429246296
+          */
+
+        curl_setopt($curl_handle, CURLOPT_URL,'https://api.telegram.org/bot' . $this->telegram_token . '/sendMessage?chat_id=-244481806&text=Hey%20Noury!%20Je%20hebt%20zometeen%20' . str_replace($search, $replace, $lesson["subjects"][0]) . '%20in%20lokaal%20' . $lesson['locations'][0] .'.%20📚⏰');
+        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Slurx API requester');
+        $query = curl_exec($curl_handle);
+        curl_close($curl_handle);
+
+        var_dump($query); echo "<br /><br />";
       }
     }
   }
